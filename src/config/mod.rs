3cae324efe85d3eaf68b110e12ad3e67f27df6dc
Linus Keiser <linus@keiser.co>
2023-04-17 15:47:39 +0200

    feat: add some experiments with password hashing

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -6,6 +6,8 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
     pub version: String,
+    pub user: String,
+    pub course: String,
     pub exercises_dir: std::path::PathBuf,
     pub settings: settings::Settings,
 }
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,14 @@ use std::path::PathBuf;
 
 use clap::{Parser, Subcommand};
 
+use argon2::{
+    password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+    Argon2,
+};
+
 #[derive(Debug, Parser)]
 struct Cli {
+    /// Use a custom config
     #[arg(short, long)]
     config: Option<PathBuf>,
 
@@ -19,17 +25,55 @@ enum Commands {
         #[arg(short, long)]
         print_cli: bool,
     },
-    /// Downloads an exercise and saves it in the exercise directory
+    /// TODO: 1. token login 2. select course from list of options
+    Login {
+        username: String,
+        // password: String,
+        // course: String,
+    },
+    /// Download an exercise and save it in the exercise directory
     Download {
         #[arg(short, long)]
         url: String,
     },
-    Submit {
-        path: PathBuf,
-    },
-    Test {
-        path: PathBuf,
-    },
+    /// Submit an exercise to SmartBeans
+    Submit { path: PathBuf },
+    /// Run the tests for a local exercise
+    Test { path: PathBuf },
+}
+
+/// Hash password using Argon2
+fn hash_password(password: &str) -> anyhow::Result<String> {
+    let salt = SaltString::generate(&mut OsRng);
+
+    // Argon2 with default params (Argon2id v19)
+    let argon2 = Argon2::default();
+
+    // Hash password to PHC string ($argon2id$v=19$...)
+    let password_hash = argon2
+        .hash_password(password.as_bytes(), &salt)?
+        .to_string();
+
+    // Verify password against PHC string.
+    //
+    // NOTE: hash params from `parsed_hash` are used instead of what is configured in the
+    // `Argon2` instance.
+    // let parsed_hash = PasswordHash::new(&password_hash).unwrap();
+    // assert!(Argon2::default()
+    //     .verify_password(password.as_bytes(), &parsed_hash)
+    //     .is_ok());
+
+    Ok(password_hash)
+}
+
+/// TODO store the password securely.
+/// TODO: token login. Needs changes to the web app.
+fn login(username: &str) -> anyhow::Result<()> {
+    let password = rpassword::prompt_password("Password: ")?;
+    println!("your password: {}", password);
+    println!("hashed: {}", hash_password(&password)?);
+
+    Ok(())
 }
 
 fn main() {
@@ -40,6 +84,9 @@ fn main() {
             if *print_cli {
                 dbg!(cli);
             }
+        }
+        Some(Commands::Login { username }) => {
+            let _ = login(username);
         }
 
         None => {}
