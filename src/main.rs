d7a871d581618417c76853371c642ce17513adde
Linus Keiser <linus@keiser.co>
2023-04-17 14:54:05 +0200

    feat: add CLI interface skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,49 @@
 mod config;
 
-use clap::Parser;
+use std::path::PathBuf;
 
-#[derive(Parser)]
-struct Cli {}
+use clap::{Parser, Subcommand};
 
-fn main() {}
+#[derive(Debug, Parser)]
+struct Cli {
+    #[arg(short, long)]
+    config: Option<PathBuf>,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    Dbg {
+        #[arg(short, long)]
+        print_cli: bool,
+    },
+    /// Downloads an exercise and saves it in the exercise directory
+    Download {
+        #[arg(short, long)]
+        url: String,
+    },
+    Submit {
+        path: PathBuf,
+    },
+    Test {
+        path: PathBuf,
+    },
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Dbg { print_cli }) => {
+            if *print_cli {
+                dbg!(cli);
+            }
+        }
+
+        None => {}
+
+        _ => {}
+    }
+}
