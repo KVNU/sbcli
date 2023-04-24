0c714f8d5a842570a13e34ddcf26638e3d7107ee
Linus Keiser <linus@keiser.co>
2023-04-24 17:37:57 +0200

    fix: can't prompt when piping

--- install.sh
+++ install.sh
@@ -9,19 +9,19 @@ BINARY_NAME="sbcli"
 # Detect the platform
 platform=""
 case "$(uname -s)" in
-  Darwin)
-    platform="x86_64-apple-darwin"
-    ;;
-  Linux)
-    platform="x86_64-unknown-linux-gnu"
-    ;;
-  MINGW* | MSYS* | CYGWIN*)
-    platform="x86_64-pc-windows-msvc"
-    ;;
-  *)
-    echo "Unsupported platform: $(uname -s)"
-    exit 1
-    ;;
+Darwin)
+  platform="x86_64-apple-darwin"
+  ;;
+Linux)
+  platform="x86_64-unknown-linux-gnu"
+  ;;
+MINGW* | MSYS* | CYGWIN*)
+  platform="x86_64-pc-windows-msvc"
+  ;;
+*)
+  echo "Unsupported platform: $(uname -s)"
+  exit 1
+  ;;
 esac
 
 echo "Platform detected: $platform"
@@ -29,9 +29,7 @@ echo "Platform detected: $platform"
 # Fetch the latest release version
 latest_release_url="https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases/latest"
 
-latest_release_tag=$(curl -sL $latest_release_url | jq -r '.tag_name')
-
-if command -v jq > /dev/null 2>&1; then
+if command -v jq >/dev/null 2>&1; then
   # Use jq if available
   latest_release_tag=$(curl -sL $latest_release_url | jq -r '.tag_name')
 else
@@ -52,27 +50,22 @@ echo "Downloading binary from: $binary_url"
 curl -sL -o $BINARY_NAME $binary_url
 chmod +x $BINARY_NAME
 
-# Prompt the user to move the binary to PATH
-read -p "Do you want to move the binary to your PATH? (y/n) " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  if [[ $platform == "x86_64-pc-windows-msvc" ]]; then
-    install_dir="$HOME/.local/bin"
-    mkdir -p "$install_dir"
-  else
-    install_dir="/usr/local/bin"
-  fi
+# Try to move the binary to a directory in PATH
+if [[ $platform == "x86_64-pc-windows-msvc" ]]; then
+  install_dir="C:/Program Files/$REPO_NAME"
+  mkdir -p "$install_dir"
+  echo "Moving binary to $install_dir"
 
-  # Check if the user has write permissions to the target directory
-  if [[ -w $install_dir ]]; then
-    echo "Moving binary to $install_dir"
-    mv $BINARY_NAME $install_dir
+  if mv $BINARY_NAME $install_dir 2>/dev/null; then
     echo "Installation completed. The binary is located at $install_dir/$BINARY_NAME"
   else
-    echo "Error: You don't have write permissions to $install_dir."
-    echo "Please run this script with administrative privileges or move the binary manually."
-    exit 1
+    echo "Failed to move binary. Please run this script as an administrator."
   fi
+
 else
-  echo "Installation completed. The binary is located at ./$BINARY_NAME"
+  install_dir="/usr/local/bin"
+  echo "Moving binary to $install_dir with elevated privileges"
+  sudo mv $BINARY_NAME $install_dir
+
+  echo "Installation completed. The binary is located at $install_dir/$BINARY_NAME"
 fi
